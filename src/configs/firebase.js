c74b8160cf47ac029cae3b88838cec135c92cd9b
Rafael Zanella <rafaels.zanella@gmail.com>
2022-05-18 23:32:01 -0300

    feat: adicionar firestore cache

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -35,15 +35,4 @@ enableIndexedDbPersistence(db)
     }
   })
 
-// enableIndexedDbPersistence(db)
-//   .catch(err => {
-//     if (err.code == 'failed-precondition') {
-//       // probably multiple tabs open at once
-//       console.log('persistence failed')
-//     } else if (err.code == 'unimplemented') {
-//       // lack of browser support
-//       console.log('persistence is not available')
-//     }
-//   })
-
 export { db }
