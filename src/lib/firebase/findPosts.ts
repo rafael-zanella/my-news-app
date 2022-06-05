68abce28bdecb2aa8c625236ac4547a301493ec0
Rafael Zanella <rafaels.zanella@gmail.com>
2022-06-04 23:12:53 -0300

    feat: adicionar filtro por titulo

--- src/lib/firebase/findPosts.ts
+++ src/lib/firebase/findPosts.ts
@@ -18,10 +18,10 @@ export const findPosts = async ({ category, title }: IFindPostsParams): Promise<
   const queryConstraints = []
   const postsFromDB: any = []
 
-  if (category) queryConstraints.push(where('category', '==', `${category}`))
+  if (category) queryConstraints.push(where('category', '==', `${category}`), orderBy('createdAt', 'desc'))
   if (title) queryConstraints.push(where('title', '>=', `${title}`), where('title', '<=', `${title}\uf8ff`))
 
-  const postsQuery = query(postsRef, ...queryConstraints, orderBy('createdAt', 'desc'))
+  const postsQuery = query(postsRef, ...queryConstraints)
   const querySnapshot = await getDocs(postsQuery)
 
   querySnapshot.forEach(async (doc) => {
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,31 @@
-import { mockNews } from '@/shared/mocks/posts.mocks'
 import { Search as SearchTemplate } from '@/templates/Search'
+import { baseUrl } from '@/configs/server'
+import { useState } from 'react'
+import { IPost } from '@/shared/types/post.types'
+
+// TODO: put this method in a service folder
+const findPostsByTitle = async (title: string) => {
+  const data = await fetch(`${baseUrl}/api/findPosts?title=${title}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  return data
+}
 
 const Search = () => {
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  const onChangeFilter = async (title: any) => {
+    const res = await findPostsByTitle(title)
+    const data = await res.json()
+    setPosts(data.posts)
+  }
+
   return (
-    <SearchTemplate onChangeFilter={() => {}} posts={mockNews} />
+    <SearchTemplate onChangeFilter={onChangeFilter} posts={posts} />
   )
 }
 
